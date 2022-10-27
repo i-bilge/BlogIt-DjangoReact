c7c34d17d5e892cc3b35132cbb0246f3f619ccbd
i-bilge <iismailbilge@gmail.com>
2022-10-27 14:38:21 +0200

    serializers.py is added.

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Article
 
+
+@admin.register(Article)
 # Register your models here.
 class ArticleModel(admin.ModelAdmin):
     list_filter = ('title', 'description')
