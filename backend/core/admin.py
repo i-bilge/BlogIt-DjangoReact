898416fb90b862bb617189bd923d14b68b24e3cd
i-bilge <iismailbilge@gmail.com>
2022-10-27 14:22:09 +0200

    article model added.

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Article
 
 # Register your models here.
+class ArticleModel(admin.ModelAdmin):
+    list_filter = ('title', 'description')
+    list_display = ('title', 'description')
+    
