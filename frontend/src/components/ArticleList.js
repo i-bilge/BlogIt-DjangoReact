e782ba3526a15116b9e51a8fba9cd3a6ca073000
i-bilge <iismailbilge@gmail.com>
2022-10-27 20:31:10 +0200

    article list is done

--- frontend/src/components/ArticleList.js
+++ frontend/src/components/ArticleList.js
@@ -8,6 +8,13 @@ function ArticleList(props) {
           <div key={article.id}>
             <h1>{article.title}</h1>
             <p>{article.description}</p>
+
+            <div>
+                <button className='btn'>Update</button>
+                <button className='btn'>Delete</button>
+            </div>
+
+            <hr className='post_line'/>
           </div>
         )
       })}
